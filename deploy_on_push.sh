4c031486e0121228fdb82a6d75a6a5401d3a743d
5-pebbles <5-pebble@protonmail.com>
2025-02-17 23:07:29 -0500

    ci/cd(authority): add digital ocean droplet to deployment list;

--- deploy_on_push.sh
+++ deploy_on_push.sh
@@ -35,6 +35,7 @@ function deploy_zip() {
 # Format: [user host secret_path]
 declare -a deployments=(
   "ghostbird acceptance.auxv.org top_secret_do_not_share/acceptance.key"
+  "root authority.auxv.org top_secret_do_not_share/authority.key"
 )
 
 
